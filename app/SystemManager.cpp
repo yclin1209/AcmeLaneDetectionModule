246cb2a48050816d053f1da919ba372ae7239597
indushekhar <indushekhar05@gmail.com>
2018-10-16 07:01:00 -0400

    [13][SMC] Implemented SystemManager class

--- app/SystemManager.cpp
+++ app/SystemManager.cpp
@@ -7,26 +7,42 @@
 * @copyright MIT License (c) 2018 Indushekhar Singh
 */
 
-#include <string>
-#include <vector>
-#include "opencv2/opencv.hpp"
-#include "../include/SystemManager.hpp"
-#include <opencv2/highgui/highgui.hpp>
-
-
 SystemManager::SystemManager(){}
 SystemManager::~SystemManager(){}
 
 int SystemManager::runLane(std::string filename, int no_of_frames) {
+  cv::VideoCapture capture(filename);
+  if (!capture.isOpened())
+    return -1;
+  cv::Mat frame;
+  for (int i= 0 ; i < no_of_frames ; i++){
 
-  // read the video from VideoCapture
-  // Check if frame is captured
-  // run loop till 1500 frames
-  // check if frames are avialable
-  // do the lane detection
+    if (!capture.read(frame)) {
+      std::cout<<"No frame to process";
+      break;
+    }
+    cv::Mat image_filter = lanedetection_.filter(frame);
+    cv::Mat image_edge = lanedetection_.edgeDetector(image_filter);
+    cv::Mat image_roi = lanedetection_.roiExtract(image_edge);
+    std::vector<cv::Vec4i>  lines = lanedetection_.getLines(image_roi);
 
+    if (!lines.empty()) {
+      std::vector<cv::Point>  lane = lanedetection_.lineFitting(lines, image_roi);
+      std::string turn = lanedetection_.turnPrediction(10);
+      double drive_heading = lanedetection_.driveHeading();
+      plot_.plot(frame, lane, turn,drive_heading);
+      cv::waitKey(20);
+    }
+    else {
+      std::cout<<" NO lane Detected"<<std::endl;
+    }
+
+  }
+  cv::destroyAllWindows();
   return 0;
 }
+
+
 
 
 
